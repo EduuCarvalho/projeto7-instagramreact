c063d987315e90d2a8fe2c1875002e531744ce98
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-09-16 16:07:28 -0300

    Projeto completo em react falta funções

---
+++ src/Stories.js
@@ -0,0 +1,83 @@
+
+
+export default function Stories() {
+    return (
+        <div class="stories">
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/9gag.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    9gag
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/meowed.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    meowed
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/barked.svg" alt = "story" />
+                </div>
+                <div class="usuario">
+                    barked
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/nathanwpylestrangeplanet.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    nathanwpylestrangeplanet
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/wawawicomics.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    wawawicomics
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/respondeai.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    respondeai
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/filomoderna.svg" alt = "story" />
+                </div>
+                <div class="usuario">
+                    filomoderna
+                </div>
+            </div>
+
+            <div class="story">
+                <div class="imagem">
+                    <img src="img/memeriagourmet.svg" alt = "story"/>
+                </div>
+                <div class="usuario">
+                    memeriagourmet
+                </div>
+            </div>
+
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    )
+}
---
+++ src/Sugestoes.js
@@ -0,0 +1,73 @@
+
+
+export default function Sugestoes() {
+    return (
+        <div class="sugestoes">
+            <div class="titulo">
+                Sugestões para você
+                <div>Ver tudo</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="img/bad.vibes.memes.svg" alt = "usuario"/>
+                    <div class="texto">
+                        <div class="nome">bad.vibes.memes</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="img/chibirdart.svg" alt = "stosugestao"/>
+                    <div class="texto">
+                        <div class="nome">chibirdart</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="img/razoesparaacreditar.svg" alt = "stosugestao"/>
+                    <div class="texto">
+                        <div class="nome">razoesparaacreditar</div>
+                        <div class="razao">Novo no Instagram</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="img/adorable_animals.svg" alt = "stosugestao"/>
+                    <div class="texto">
+                        <div class="nome">adorable_animals</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+
+            <div class="sugestao">
+                <div class="usuario">
+                    <img src="img/smallcutecats.svg" alt = "stosugestao"/>
+                    <div class="texto">
+                        <div class="nome">smallcutecats</div>
+                        <div class="razao">Segue você</div>
+                    </div>
+                </div>
+
+                <div class="seguir">Seguir</div>
+            </div>
+        </div>
+
+    );
+}
